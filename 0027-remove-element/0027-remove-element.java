24bde5d80b86c85063c8390a9de06808405fca5a
SHAM PATIL <shampatil7275@gmail.com>
2026-07-27 14:44:23 +0530

    Time: 0 ms (100%), Space: 43.5 MB (62.21%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.java
@@ -0,0 +1,23 @@
+    class Solution {
+        public int removeElement(int[] nums, int val) {
+
+            int n=nums.length;
+            int left=0;
+            int right=n-1;
+            
+        while(left<=right){
+
+        if(nums[left]==val){
+            int temp=nums[left];
+            nums[left]=nums[right];
+            nums[right]=temp;
+            right--;
+        }
+        else{
+            left++;
+        }
+        }
+            return left;
+
+        }
+    }
